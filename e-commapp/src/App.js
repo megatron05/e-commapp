c77592540119d3b133120465278a0569e1622cbd
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-01-07 14:31:38 -0600

    new changes for app

---
+++ e-commapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginandSignUp from './Components/LoginSignup/LoginandSignUp';
+
+function App() {
+  return (
+    <div>
+      <LoginandSignUp/>
+    </div>
+  );
+}
+
+export default App;
